f3ec0eacb3181eb98ff7cb85fd01d65db152a58c
Roelof Kemp <rkemp@cs.vu.nl>
2013-08-28 11:19:00 +0200

    Initial import

---
+++ gen/interdroid/swan/cuckoo_station_sensor/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package interdroid.swan.cuckoo_station_sensor;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
